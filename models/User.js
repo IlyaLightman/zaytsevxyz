7a52198fec6372c0021a37e30a3afd49a4712730
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-05-26 15:11:36 +0300

    user initial model and auth router

---
+++ models/User.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose')
+
+const schema = new Schema({
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	nickname: {
+		type: String,
+		required: true,
+		unique: true
+	}
+})
+
+module.exports = model('User', schema)
---
+++ routes/auth.routes.js
@@ -0,0 +1,19 @@
+const { Router } = require('express')
+const { hash, compare } = require('bcryptjs')
+const config = require('config')
+const jwt = require('jsonwebtoken')
+// const User
+const {} = require('express-validator')
+const {} = require('../utils/validators')
+
+const router = Router()
+
+// /api/auth/register
+router.post('/register',
+	async (req, res) => {
+	try {
+
+	} catch (err) {
+
+	}
+})
