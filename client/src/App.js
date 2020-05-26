8b5249a4aed4f6a8703280114350e6f5ecd38609
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-05-26 17:37:13 +0300

    auth context and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import useAuth from './hooks/auth.hook'
+import { AuthContext } from './context/AuthContext'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>zaytsev.xyz</h1>
-    </div>
-  );
+	const { token, login, logout, userId, ready } = useAuth()
+	const isAuthenticated = !!token
+
+	return (
+		// <div className="App">
+		//   <h1>zaytsev.xyz</h1>
+		// </div>
+		<AuthContext.Provider value={{
+			token, login, logout, userId, isAuthenticated
+		}}>
+			<Switch>
+				<Redirect to='/login' />
+			</Switch>
+		</AuthContext.Provider>
+	)
 }
 
-export default App;
+export default App
