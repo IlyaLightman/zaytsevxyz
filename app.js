9b24828821d71fb8026281749c5f1811235eb2c4
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-05-26 14:41:55 +0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
+const mongoose = require('mongoose')
 const express = require('express')
+const config = require('config')
+
+const app = express()
+
+app.use(express.json({ extended: true }))
+
+const PORT = config.get('PORT')
+
+async function start() {
+	try {
+		await mongoose.connect(config.get('MONGO_URI'), {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true
+		})
+
+		app.listen(PORT, () => console.log(`App has been started on port ${PORT}... `))
+	} catch (err) {
+		console.log('Server Error', err.message)
+		process.exit(1)
+	}
+}
+
+start()
+	.then(() => console.log('Success'))
