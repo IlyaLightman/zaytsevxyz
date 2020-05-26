fc1e0492bdceca117d8f2472a2cfeaf0cbcc61dc
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-05-26 18:58:11 +0300

    initial store and main page setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,18 @@ import './App.css'
 import useAuth from './hooks/auth.hook'
 import { AuthContext } from './context/AuthContext'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import MainPage from './containers/MainPage/MainPage'
 
 function App() {
 	const { token, login, logout, userId, ready } = useAuth()
 	const isAuthenticated = !!token
 
 	return (
-		// <div className="App">
-		//   <h1>zaytsev.xyz</h1>
-		// </div>
 		<AuthContext.Provider value={{
 			token, login, logout, userId, isAuthenticated
 		}}>
 			<Switch>
+				<Route path='/' component={MainPage} />
 				<Redirect to='/login' />
 			</Switch>
 		</AuthContext.Provider>
