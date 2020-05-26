adc52d4577e2b3d8c71c8cdaacaa48e50ead601f
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-05-26 17:09:14 +0300

    react init

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,18 +2,83 @@ const { Router } = require('express')
 const { hash, compare } = require('bcryptjs')
 const config = require('config')
 const jwt = require('jsonwebtoken')
-// const User
-const {} = require('express-validator')
-const {} = require('../utils/validators')
+const User = require('../models/User')
+const { validationResult } = require('express-validator')
+const { registerValidators, loginValidators } = require('../utils/validators')
 
 const router = Router()
 
 // /api/auth/register
-router.post('/register',
+router.post('/register', registerValidators,
 	async (req, res) => {
-	try {
+		try {
+			const errors = validationResult(req)
 
-	} catch (err) {
+			if (!errors.isEmpty()) {
+				return res.status(400).json({
+					errors: errors.array(),
+					message: 'Incorrect registration data'
+				})
+			}
 
+			const { email, password, nickname } = req.body
+
+			const candidateByEmail = await User.findOne({ email })
+			const candidateByNickName = await User.findOne({ nickname })
+			if (candidateByEmail || candidateByNickName) {
+				return res.status(400).json({
+
+				})
+			}
+
+			const hashedPassword = await hash(password, 12)
+			const user = new User({ email, password: hashedPassword, nickname })
+
+			user.save()
+
+			res.status(201).json({ message: 'The user was created successfully' })
+		} catch (err) {
+			res.status(500).json({ message: 'Something wrong' })
+		}
 	}
-})
+)
+
+// /api/auth/login
+router.post('/login', loginValidators,
+	async (req, res) => {
+		try {
+			const errors = validationResult(req)
+			if (!errors.isEmpty()) {
+				return res.status(400).json({
+
+				})
+			}
+
+			const { email, password } = req.body
+
+			const user = await User.findOne({ email })
+			if (!user) {
+				return res.status(400).json({ message: 'There is no such user' })
+			}
+
+			const isMatch = await compare(password, user.password)
+			if (!isMatch) {
+				return res.status(400).json({
+
+				})
+			}
+
+			const token = jwt.sign(
+				{ userId: user.id },
+				config.get('jwtSecret'),
+				{ expiresIn: '1h' }
+			)
+
+			res.json({ token, userId: user.id })
+		} catch (err) {
+			res.status(500).json({ message: 'Something wrong' })
+		}
+	}
+)
+
+module.exports = router
--- utils/validators.js
+++ utils/validators.js
@@ -1,2 +1,53 @@
 const { body } = require('express-validator')
 const User = require('../models/User')
+
+exports.registerValidators = [
+	body('email', 'Incorrect Email or user with this Email already exists')
+		.isEmail()
+		.custom(async (value, { req }) => {
+			try {
+				const user = await User.findOne({ email: value })
+				if (user) {
+					return Promise.reject('User with this Email already exists')
+				}
+				return true
+			} catch (err) {
+				console.log(err)
+			}
+		}),
+	body('password', 'The password has to have minimum six symbols')
+		.isLength({ min: 6, max: 26 })
+		.isAlphanumeric()
+		.trim(),
+	body('nickname', 'This nickname is incorrect')
+		.isLength({ min: 2, max: 16 })
+		.isAlphanumeric(),
+	body('confirm', `Passwords don't match`)
+		.custom((value, { req }) => {
+			if (value !== req.body.password) {
+				throw new Error(`Passwords don't match`)
+			}
+			return true
+		})
+		.trim()
+]
+
+exports.loginValidators = [
+	body('email', 'Incorrect Email')
+		.isEmail()
+		.custom(async (value, { req }) => {
+			try {
+				const user = await User.findOne({ email: value })
+				if (!user) {
+					return Promise.reject('There is no user with this Email')
+				}
+			} catch (err) {
+				console.log(err)
+			}
+		})
+		.normalizeEmail(),
+	body('password', 'Incorrect password')
+		.isLength({ min: 6, max: 26 })
+		.isAlphanumeric()
+		.trim()
+]
