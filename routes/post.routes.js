6f04f7caf1696287e18e1e4c5b112bcfc1c5976d
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-05-30 21:38:33 +0300

    post router and auth checker middleware

---
+++ routes/post.routes.js
@@ -0,0 +1,48 @@
+const { Router } = require('express')
+const Post = require('../models/Post')
+const auth = require('../middleware/auth.middleware')
+
+const router = Router()
+const errorMessage = 'Что-то пошло не так.... попробуйте снова'
+
+router.post('/create', auth, async (req,res) => {
+	try {
+		const {
+			title, cover, content, user, date, tags
+		} = req.body
+
+		if (!user.isAdmin) {
+			res.status(500).json({
+				message: 'У вас нет прав'
+			})
+		}
+
+		const author = {
+			name: user.nickname,
+			userId: user._id
+		}
+		const post = new Post(
+			title, cover, content, author, date, tags
+		)
+
+		await post.save()
+
+		res.status(201).json({
+			message: title
+		})
+	} catch (err) {
+		res.status(500).json({
+			message: errorMessage
+		})
+	}
+})
+
+router.get('/', async (req, res) => {
+	try {
+
+	} catch (err) {
+		res.status(500).json({
+			message: errorMessage
+		})
+	}
+})
