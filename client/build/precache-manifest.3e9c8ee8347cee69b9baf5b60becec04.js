cff6abdc788c7e3341a99a5b7ef06fd6b58f3296
Ilya Zaytsev <ilya@zaytsev.xyz>
2020-06-18 21:07:17 +0300

    client build for the server

---
+++ client/build/precache-manifest.3e9c8ee8347cee69b9baf5b60becec04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc4f69d1a0b62f16f05c0e55b53d390c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "123878d2a10ef2d4d1fe",
+    "url": "/static/css/main.737f5eb8.chunk.css"
+  },
+  {
+    "revision": "44a4f3453718e91974f5",
+    "url": "/static/js/2.9a7dc1f3.chunk.js"
+  },
+  {
+    "revision": "e1c104071915bf7908724d588de26e45",
+    "url": "/static/js/2.9a7dc1f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "123878d2a10ef2d4d1fe",
+    "url": "/static/js/main.24fcb6e1.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
